e03e734db08c9626cbfa0051901990c1ff7fa338
rudransh02 <rudranshsinghrajpurohit@gmai.com>
2022-11-22 13:40:53 +0530

    Code Of Bubble Sort Using Array Added

---
+++ BubbleSortUsingArray.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+// 6,2,8,10,4 -> 2,6,8,10,4 -> 2,6,8,4,10 -> 2,6,4,8,10 -> 2,4,6,8,10
+
+void BubbleSort(int array[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i + 1; j < size; j++)
+        {
+            if (array[i] > array[j])
+            {
+                swap(array[i], array[j]);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int size = 10;
+    int array[size] = {5, 14, 663, 2, 47, 69, 48, 63, 41, 255};
+    BubbleSort(array, size);
+    for (int i = 0; i < size; i++)
+    {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
