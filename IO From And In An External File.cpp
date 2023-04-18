6662eb7b975fb1eee692fba44f0d789c18357763
rudransh02 <rudranshsinghrajpurohit@gmai.com>
2023-04-19 01:39:52 +0530

    Code Of Reading/Writing Input/Output From An External File Added

---
+++ IO From And In An External File.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // If Online Judge Is Not Defined Use Input And Output Files For Execution
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+#endif
+}
